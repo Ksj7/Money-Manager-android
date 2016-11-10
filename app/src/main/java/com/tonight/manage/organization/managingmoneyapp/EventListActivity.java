f6d969513542de6e99e7a19e68d019ec27a29f81
Ksj7 <lobstar733@gmail.com>
2016-11-10 17:32:41 +0900

    Fix eventList Add Navigation Drawer, Redesigned

--- app/src/main/java/com/tonight/manage/organization/managingmoneyapp/EventListActivity.java
+++ app/src/main/java/com/tonight/manage/organization/managingmoneyapp/EventListActivity.java
@@ -2,12 +2,18 @@ package com.tonight.manage.organization.managingmoneyapp;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,7 +25,7 @@ import java.util.ArrayList;
 /**
  * Created by sujinKim on 2016-11-04.
  */
-public class EventListActivity extends AppCompatActivity{
+public class EventListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView mEventListRecyclerView;
     private EventListActivity.EventListAdapter mEventListAdapter;
     private SwipeRefreshLayout mEventListSwipeRefreshLayout;
@@ -48,6 +54,70 @@ public class EventListActivity extends AppCompatActivity{
             }
         });
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_setting) {
+            // Handle the camera action
+        } else if (id == R.id.nav_alarm_list) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
@@ -82,7 +152,7 @@ public class EventListActivity extends AppCompatActivity{
             //test
             holder.eventName.setText("1학기 회비");
             holder.eventNumber.setText("82명");
-            holder.eventpercent.setText("82명");
+            holder.eventpercent.setText("50%");
 
             //이게 정상
             //holder.groupName.setText(groupDatas.get(position).groupName);
