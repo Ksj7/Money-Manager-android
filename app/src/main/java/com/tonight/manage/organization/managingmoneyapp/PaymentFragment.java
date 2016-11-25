6617dc973f3b88e113a6374421d89bee636e7634
SoHyang.Jeon <SoHyang.Jeon126@gmail.com>
2016-11-25 19:56:10 +0900

    add PopupMenu in eventInfo Activity

--- app/src/main/java/com/tonight/manage/organization/managingmoneyapp/PaymentFragment.java
+++ app/src/main/java/com/tonight/manage/organization/managingmoneyapp/PaymentFragment.java
@@ -2,11 +2,14 @@ package com.tonight.manage.organization.managingmoneyapp;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,6 +28,7 @@ public class PaymentFragment extends Fragment {
     private EventInfoPaymentAdapter mPaymentListAdapter;
     private SwipeRefreshLayout mPaymentListSwipeRefreshLayout;
     // private ScrollView mPaymentListScrollView;
+    private FloatingActionButton mFabButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -58,6 +62,24 @@ public class PaymentFragment extends Fragment {
             }
         });
 
+
+        mFabButton = (FloatingActionButton) v.findViewById(R.id.eventInfo_addmoney_btn);
+        mFabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(getActivity(), mFabButton);
+                popup.getMenuInflater().inflate(R.menu.activity_eventinfo_popup, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        return false;
+                    }
+                });
+                popup.show();
+            }
+        });
 
         return v;
     }
