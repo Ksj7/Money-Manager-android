c02b9f2a87c831bcc22ed8fd5e7e0adb8d71b1f4
Youngho <hooo5513@naver.com>
2016-12-02 21:23:54 +0900

    Create group

--- app/src/main/java/com/tonight/manage/organization/managingmoneyapp/Custom/CustomCreateGroupPopup.java
+++ app/src/main/java/com/tonight/manage/organization/managingmoneyapp/Custom/CustomCreateGroupPopup.java
@@ -1,46 +1,92 @@
 package com.tonight.manage.organization.managingmoneyapp.Custom;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
 import com.tonight.manage.organization.managingmoneyapp.GroupListActivity;
 import com.tonight.manage.organization.managingmoneyapp.R;
+import com.tonight.manage.organization.managingmoneyapp.Server.NetworkDefineConstant;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Created by sujinKim on 2016-11-04.
  */
 
 public class CustomCreateGroupPopup extends DialogFragment {
+    String groupname;
+    String account;
+    String bank;
+    String balance;
+    String id;
+
+    boolean isSuccess;
+    EditText roomCodeEditText;
+    EditText accountEditText;
+    EditText bankNameEditText;
+    EditText balanceEditText;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStyle(STYLE_NO_TITLE,R.style.CustomDialogTheme);
+        setStyle(STYLE_NO_TITLE, R.style.CustomDialogTheme);
     }
 
-    public static CustomCreateGroupPopup newInstance() {
-        return new CustomCreateGroupPopup();
+    public static CustomCreateGroupPopup newInstance(String id) {
+        CustomCreateGroupPopup groupPopup = new CustomCreateGroupPopup();
+        Bundle b = new Bundle();
+        b.putString("id", id);
+        groupPopup.setArguments(b);
+        return groupPopup;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.popup_create_group,container,false);
+        View view = inflater.inflate(R.layout.popup_create_group, container, false);
+        Bundle b = getArguments();
+        if (b == null) {
+            Toast.makeText(getActivity(), "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+            return view;
+        }
+
+        id = b.getString("id");
+
         Button Ybtn = (Button) view.findViewById(R.id.confirmBtn);
         Button NBtn = (Button) view.findViewById(R.id.negativeBtn);
+        roomCodeEditText = (EditText) view.findViewById(R.id.roomCodeEdit);
+        accountEditText = (EditText) view.findViewById(R.id.accountEdit);
+        bankNameEditText = (EditText) view.findViewById(R.id.balanceEdit);
+        balanceEditText = (EditText) view.findViewById(R.id.balanceEdit);
+
         Ybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "그룹이 개설되었습니다.", Toast.LENGTH_SHORT).show();
-                GroupListActivity activity = (GroupListActivity) getActivity();
-                activity.isSuccessCreateGroup(true);
-                dismiss();
+                groupname = roomCodeEditText.getText().toString();
+                account = accountEditText.getText().toString();
+                bank = bankNameEditText.getText().toString();
+                balance = balanceEditText.getText().toString();
+                new CreateGroupAsyncTask().execute(id, groupname, bank, account, balance);
+
             }
 
         });
@@ -56,6 +102,13 @@ public class CustomCreateGroupPopup extends DialogFragment {
 
     }
 
+    private void clearSetting() {
+        roomCodeEditText.setText("");
+        accountEditText.setText("");
+        bankNameEditText.setText("");
+        balanceEditText.setText("");
+    }
+
     @Override
     public void onStop() {
         super.onStop();
@@ -67,6 +120,75 @@ public class CustomCreateGroupPopup extends DialogFragment {
         super.onActivityCreated(savedInstanceState);
         int width = getResources().getDimensionPixelSize(R.dimen.popup_create_group_width);
         int height = getResources().getDimensionPixelSize(R.dimen.popup_create_group_height);
-        getDialog().getWindow().setLayout(width,height);
+        getDialog().getWindow().setLayout(width, height);
+    }
+
+    public class CreateGroupAsyncTask extends AsyncTask<String, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String requestURL = "";
+            Response response = null;
+            try {
+                requestURL = NetworkDefineConstant.SERVER_URL_GROUP_LIST;
+
+                OkHttpClient toServer = NetworkDefineConstant.getOkHttpClient();
+                FormBody.Builder builder = new FormBody.Builder();
+                builder.add("signal", "2")
+                        .add("userid", params[0])
+                        .add("groupname", params[1])
+                        .add("bank", params[2])
+                        .add("account", params[3])
+                        .add("balance", params[4]);
+
+                FormBody formBody = builder.build();
+
+                Request request = new Request.Builder()
+                        .url(requestURL)
+                        .post(formBody)
+                        .build();
+
+                response = toServer.newCall(request).execute();
+                boolean flag = response.isSuccessful();
+                ResponseBody resBody = response.body();
+
+                if (flag) {
+                    String valid = resBody.string();
+                    if (valid.contains("0"))
+                        isSuccess = true;
+                    else
+                        isSuccess = false;
+
+                } else {
+                    Log.e("에러", "그룹 개설 실패");
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } finally
+            {
+                if (response != null) {
+                    response.close();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (isSuccess) {
+                Toast.makeText(getActivity(), "그룹이 개설되었습니다.", Toast.LENGTH_SHORT).show();
+                GroupListActivity activity = (GroupListActivity) getActivity();
+                activity.isSuccessCreateGroup(true);
+                dismiss();
+            } else {
+                Toast.makeText(getActivity(), "그룹 생성 실패", Toast.LENGTH_SHORT).show();
+                clearSetting();
+                return;
+            }
+        }
+
     }
 }
